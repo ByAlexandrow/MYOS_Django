b2e91ac796f6a99198a8aaad966f5e5274fd1fbd
Gladia-Torr <egor.aleksx@yandex.ru>
2024-06-05 21:25:14 +0300

    Added apps

---
+++ backend/Mystery_Of_Story/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Mystery_Of_Story project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mystery_Of_Story.settings')
+
+application = get_asgi_application()
