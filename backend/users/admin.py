4b6b9d18cbb3372739f06b2143e6075b79839f6b
Gladia-Torr <egor.aleksx@yandex.ru>
2024-06-09 01:25:49 +0300

    Added one important function

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import AdminMessage
+
+
+@admin.register(AdminMessage)
+class MessageForAdmin(admin.ModelAdmin):
+    list_display = ('user', 'subject', 'timestamp',)
+    list_filter = ('user', 'subject', 'timestamp',)
