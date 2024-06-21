074a78ce9f0c7757f5e63a825a74000bfa82458d
Gladia-Torr <egor.aleksx@yandex.ru>
2024-06-21 13:37:02 +0300

    Edited book template

--- victoria/views.py
+++ victoria/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from victoria.models import Victoria
 
 
 def victoria(request):
     moments = Victoria.objects.all().order_by('created_at')
-    return render(request, 'victoria/victoria.html', {'moments': moments})
+    paginator = Paginator(moments, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'victoria/victoria.html', {'moments': page_obj})
