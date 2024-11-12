a64eba81d225d30391e4353202229ea92038eff7
Gladia-Torr <egor.aleksx@yandex.ru>
2024-11-12 13:37:15 +0300

    Edited settings

--- Mystery_Of_Story/settings.py
+++ Mystery_Of_Story/settings.py
@@ -87,12 +87,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'newdb',
-        # 'USER': 'newuser',
-        # 'PASSWORD': 'password',
-        # 'HOST': 'localhost',
-        # 'PORT': '3306',
     }
 }
 
